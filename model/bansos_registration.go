4ff640be96dfb152b958774d739cebcc21ad72bf
Willy Wilsen <13520160@std.stei.itb.ac.id>
2023-12-16 15:37:51 +0700

    Change enum to string

--- model/bansos_registration.go
+++ model/bansos_registration.go
@@ -21,15 +21,15 @@ type BansosRegistration struct {
 	Name       string     `json:"name"            gorm:"size:255;null"`
 	Nik        string     `json:"nik"             gorm:"size:255;null"`
 	NoKK       string     `json:"nokk"            gorm:"size:255;not null"`
-	Income              IncomeEnum              `json:"income"                  gorm:"type:enum('<500 ribu','500 ribu-1 juta','1 juta-1.5 juta','>1.5 juta');null"`
-	FloorArea           FloorAreaEnum           `json:"floor_area"              gorm:"type:enum('Diatas 8m²','Dibawah 8m²');null"`
-	WallQuality         WallQualityEnum         `json:"wall_quality"            gorm:"type:enum('Buruk','Normal','Bagus');null"`
-	NumberOfMeals       NumberOfMealsEnum       `json:"number_of_meals"         gorm:"type:enum('0','1','2','3');null"`
-	Fuel                FuelEnum                `json:"fuel"                    gorm:"type:enum('Kayu/Arang','Gas/LPG');null"`
-	Education           EducationEnum           `json:"education"               gorm:"type:enum('SD','SMP','SMA','Sarjana');null"`
-	TotalAsset          TotalAssetEnum          `json:"total_asset"             gorm:"type:enum('<500 ribu','500 ribu-1 juta','1 juta-1.5 juta','>1.5 juta');null"`
-	Treatment           TreatmentEnum           `json:"treatment"               gorm:"type:enum('Mampu','Tidak Mampu');null"`
-	NumberOfDependents  NumberOfDependentsEnum  `json:"number_of_dependents"    gorm:"type:enum('0','1','2','>2');null"`
+	Income             string                 `json:"income"                  gorm:"size:255;null"`
+	FloorArea          string                 `json:"floor_area"              gorm:"size:255;null"`
+	WallQuality        string                 `json:"wall_quality"            gorm:"size:255;null"`
+	NumberOfMeals      string                 `json:"number_of_meals"         gorm:"size:255;null"`
+	Fuel               string                 `json:"fuel"                    gorm:"size:255;null"`
+	Education          string                 `json:"education"               gorm:"size:255;null"`
+	TotalAsset         string                 `json:"total_asset"             gorm:"size:255;null"`
+	Treatment          string                 `json:"treatment"               gorm:"size:255;null"`
+	NumberOfDependents string                 `json:"number_of_dependents"    gorm:"size:255;null"`
 	Status     StatusEnum `json:"status"          gorm:"type:enum('ON_PROGRESS','ACCEPTED','REJECTED','TAKEN');not null;default:'ON_PROGRESS'"`
 	Point			decimal.Decimal `json:"point"      gorm:"type:decimal(10,2);not null;default:0"`
 	ApprovalAt time.Time  `json:"approval_at"     gorm:"type:datetime;autoCreateTime"`
--- model/user.go
+++ model/user.go
@@ -7,80 +7,6 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
-type IncomeEnum string
-
-const (
-	Income_500rb    IncomeEnum = "<500 ribu"
-	Income500rb_1jt IncomeEnum = "500 ribu-1 juta"
-	Income1jt_1_5jt IncomeEnum = "1 juta-1.5 juta"
-	Income1_5jt_2   IncomeEnum = ">1.5 juta"
-)
-
-type FloorAreaEnum string
-
-const (
-	FloorArea_8 FloorAreaEnum = "Diatas 8m²"
-	FloorArea8_ FloorAreaEnum = "Dibawah 8m²"
-)
-
-type WallQualityEnum string
-
-const (
-	WallQualityBad    WallQualityEnum = "Buruk"
-	WallQualityNormal WallQualityEnum = "Normal"
-	WallQualityGood   WallQualityEnum = "Bagus"
-)
-
-type NumberOfMealsEnum string
-
-const (
-	NumberOfMeals_0 NumberOfMealsEnum = "0"
-	NumberOfMeals_1 NumberOfMealsEnum = "1"
-	NumberOfMeals_2 NumberOfMealsEnum = "2"
-	NumberOfMeals_3 NumberOfMealsEnum = "3"
-)
-
-type FuelEnum string
-
-const (
-	FuelKayuArang FuelEnum = "Kayu/Arang"
-	FuelGasLPG    FuelEnum = "Gas/LPG"
-)
-
-type EducationEnum string
-
-const (
-	EducationSD      EducationEnum = "SD"
-	EducationSMP     EducationEnum = "SMP"
-	EducationSMA     EducationEnum = "SMA"
-	EducationSarjana EducationEnum = "Sarjana"
-)
-
-type TotalAssetEnum string
-
-const (
-	TotalAsset_500rb    TotalAssetEnum = "<500 ribu"
-	TotalAsset500rb_1jt TotalAssetEnum = "500 ribu-1 juta"
-	TotalAsset1jt_1_5jt TotalAssetEnum = "1 juta-1.5 juta"
-	TotalAsset1_5jt_2   TotalAssetEnum = ">1.5 juta"
-)
-
-type TreatmentEnum string
-
-const (
-	TreatmentMampu      TreatmentEnum = "Mampu"
-	TreatmentTidakMampu TreatmentEnum = "Tidak Mampu"
-)
-
-type NumberOfDependentsEnum string
-
-const (
-	NumberOfDependents0  NumberOfDependentsEnum = "0"
-	NumberOfDependents1  NumberOfDependentsEnum = "1"
-	NumberOfDependents2  NumberOfDependentsEnum = "2"
-	NumberOfDependents2_ NumberOfDependentsEnum = ">2"
-)
-
 type RoleEnum string
 
 const (
@@ -93,15 +19,15 @@ type User struct {
 	Name               string                 `json:"name"                    gorm:"size:255;not null"`
 	Nik                string                 `json:"nik"                     gorm:"size:255;null"`
 	NoKK               string                 `json:"nokk"                    gorm:"size:255;null"`
-	Income             IncomeEnum             `json:"income"                  gorm:"type:enum('<500 ribu','500 ribu-1 juta','1 juta-1.5 juta','>1.5 juta');null"`
-	FloorArea          FloorAreaEnum          `json:"floor_area"              gorm:"type:enum('Diatas 8m²','Dibawah 8m²');null"`
-	WallQuality        WallQualityEnum        `json:"wall_quality"            gorm:"type:enum('Buruk','Normal','Bagus');null"`
-	NumberOfMeals      NumberOfMealsEnum      `json:"number_of_meals"         gorm:"type:enum('0','1','2','3');null"`
-	Fuel               FuelEnum               `json:"fuel"                    gorm:"type:enum('Kayu/Arang','Gas/LPG');null"`
-	Education          EducationEnum          `json:"education"               gorm:"type:enum('SD','SMP','SMA','Sarjana');null"`
-	TotalAsset         TotalAssetEnum         `json:"total_asset"             gorm:"type:enum('<500 ribu','500 ribu-1 juta','1 juta-1.5 juta','>1.5 juta');null"`
-	Treatment          TreatmentEnum          `json:"treatment"               gorm:"type:enum('Mampu','Tidak Mampu');null"`
-	NumberOfDependents NumberOfDependentsEnum `json:"number_of_dependents"    gorm:"type:enum('0','1','2','>2');null"`
+	Income             string                 `json:"income"                  gorm:"size:255;null"`
+	FloorArea          string                 `json:"floor_area"              gorm:"size:255;null"`
+	WallQuality        string                 `json:"wall_quality"            gorm:"size:255;null"`
+	NumberOfMeals      string                 `json:"number_of_meals"         gorm:"size:255;null"`
+	Fuel               string                 `json:"fuel"                    gorm:"size:255;null"`
+	Education          string                 `json:"education"               gorm:"size:255;null"`
+	TotalAsset         string                 `json:"total_asset"             gorm:"size:255;null"`
+	Treatment          string                 `json:"treatment"               gorm:"size:255;null"`
+	NumberOfDependents string                 `json:"number_of_dependents"    gorm:"size:255;null"`
 	Email              string                 `json:"email"                   gorm:"size:255;not null;unique"`
 	Phone              string                 `json:"phone"                   gorm:"size:255;not null;unique"`
 	Password           string                 `json:"password"                gorm:"size:255;not null"`
